4e3fd25c41e19015588045b09449767067e5ac46
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-11 11:14:41 +0600

    created header, register, login components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,18 @@ const Login = () => {
     return (
         <div>
             <h3>Please Login</h3>
+            <form>
+                <input type="text" placeholder='Your Name' />
+                <br />
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+            <div style={{margin:'20px'}}>
+                <button>Google Sign In</button>
+            </div>
         </div>
     );
 };
