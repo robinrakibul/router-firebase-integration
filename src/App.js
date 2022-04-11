8262b7d5789b6352309c6f2d7b70ffa7c516edb0
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-11 11:05:08 +0600

    ibasic router created, components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header></Header>
+     <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
+     </Routes>
     </div>
   );
 }
